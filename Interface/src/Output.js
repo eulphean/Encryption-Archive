d203a7b3abdf7a1a054dbac37242fe47d15dd641
Amay Kataria <amkatari@microsoft.com>
2019-08-19 00:12:33 -0500

    Initial draw.

--- Interface/src/Output.js
+++ Interface/src/Output.js
@@ -1,13 +1,58 @@
 //[ Output Canvas                                                    ]
 // width, height are the variables set when called createCanvas in p5.js
 // when drawing in the canvas, we need to use those variables.
+class Cell {
+    constructor(xPos, yPos, color) {
+        this.col = color; 
+        this.xPos = xPos; 
+        this.yPos = yPos; 
+    }
+
+    draw() {
+        push();
+            // Translate to the center of the cell. 
+            translate(this.xPos, this.yPos);
+            fill(this.col);
+            rect(0, 0, cellSize, cellSize); // Rather than rectangles, these will be circles actually at the center of the cell. 
+        pop();
+    }
+}
 class Output {
     constructor(yPos, height) {
         this.canvas = createCanvas(displayWidth, height);
         this.canvas.position(0, yPos);  
+
+        this.cells = []; 
+        this.columns = width/cellSize; 
+        this.rows = height/cellSize; 
+
+        // Initialize encryption bed. 
+        this.initGrid(); 
     }
 
     draw() {
-        circle(width/2, height/2, 50); 
+        for (var i = 0; i < this.rows; i++) {
+            for (var j = 0; j < this.columns; j++) {
+                this.cells[i][j].draw();
+            }
+        }
+    }
+
+    initGrid() {
+        var idx = 0; 
+        for (var i = 0; i < this.rows; i++) {
+            this.cells[i] = []; // 2D array assign.
+            for (var j = 0; j < this.columns; j++) {
+              // Create cell. 
+              var xPos = j * cellSize;
+              var yPos = i * cellSize; 
+              var color = (idx%2 == 0) ? white : black;
+              var cell = new Cell(xPos, yPos, color);
+              // Push cell. 
+              this.cells[i][j] = cell;
+              idx++; 
+            }
+            idx++; 
+        }
     }
 }
--- Interface/src/main.js
+++ Interface/src/main.js
@@ -5,6 +5,7 @@
 //[ Text Input                                              Button   ]
 var title; var output; var input;
 
+// Global format variables
 var bgColor = 'black'; 
 var fontColor = 'white'; 
 var fontSize = '25px'; 
@@ -13,11 +14,19 @@ var paddingVertical = '80px';
 var fontFamily = 'Menlo-Regular'; 
 var defaultPrivateKeyLabel = 'Private Key | '; 
 
+// Encryption bed variables
+var cellSize = 10; 
+var white; 
+var black; 
+
 // AES based encrypter. 
 var encrypter; 
 
 function setup() {
   var h = displayHeight/12; 
+
+  // Colors
+  white = color(255, 255, 255); black = color(0, 0, 0);
 
   // Total 12h units of height. 
   title = new Title(h); // 1h unit high
@@ -31,6 +40,7 @@ function setup() {
 function draw() {
   // put drawing code here
   output.draw(); 
+  noLoop();
 }
 
 function onEncrypt(message) {
