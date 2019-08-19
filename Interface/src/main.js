b9293d7d5e00489a504b685ab43ecb2d95656110
Amay Kataria <amkatari@microsoft.com>
2019-08-18 20:37:25 -0500

    Implement an encryption engine with crypto-js

--- Interface/src/main.js
+++ Interface/src/main.js
@@ -11,6 +11,9 @@ var fontSize = '30px';
 var paddingHorizontal = '50px'; 
 var paddingVertical = '80px';
 
+// AES based encrypter. 
+var encrypter; 
+
 function setup() {
   var h = displayHeight/12; 
 
@@ -18,6 +21,14 @@ function setup() {
   title = new Title(h); // 1h unit high
   output = new Output(h, h * 8); // 8h units high
   input = new Input(h * 9, h * 3); // 3h unit high
+
+  // Initialize Encryption engine. 
+  encrypter = new Encrypter(); 
+
+  // Sample encryption.
+  var out = encrypter.encrypt("Hello World"); 
+  console.log(out['char']); // Store this in the database
+  console.log(out['binary']); // Print this on the screen
 }
 
 function draw() {
